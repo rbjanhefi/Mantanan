5d939202ade82fdd6d80eea74145298e0997b19c
akhyarz <61868811+akhyarz@users.noreply.github.com>
2021-12-20 23:22:10 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,18 +1,15 @@
-<?php 
-include('config/db_conn.php');
+<?php
+session_start(); 
 
+if(!isset($_SESSION['email']) ){
+  
+    header("location:view/page/login.php");
+  
+}else{
+    header('location:view/page/landingpage.php');
+  }
 
+    
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=a, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <a href="model/log_out.php">LOGOUT</a>
-</body>
-</html>
+
