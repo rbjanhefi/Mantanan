7215d6bcfb163b3f3219f7952f222743a912b05a
Erzed <72213198+Erzeed@users.noreply.github.com>
2021-12-28 09:18:14 +0700

    update password

--- view/password.php
+++ view/password.php
@@ -30,7 +30,6 @@
         <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
             integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-        <link rel="stylesheet" href="./css/changePassword.css">
         <link rel="stylesheet" href="./css/active.css">
         <link rel="stylesheet" href="./css/navbar.css">
         <link rel="stylesheet" href="./css/button.css">
